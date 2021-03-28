5ea08aea126fda8aee031ec73786653efa2ab700
Daniel Hoeflehner <daniel.hoeflehner@outlook.com>
2021-03-28 13:15:40 +0200

    Use .net 5 and run migrations on startup

--- MovieDB.Api/Startup.cs
+++ MovieDB.Api/Startup.cs
@@ -1,6 +1,7 @@
 using System;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -44,7 +45,7 @@ namespace MovieDB.Api
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, AppDbContext context)
         {
-            //context.Database.Migrate();
+            context.Database.Migrate();
 
             if (env.IsDevelopment())
             {
