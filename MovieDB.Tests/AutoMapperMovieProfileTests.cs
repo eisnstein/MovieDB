2814aac17df83a5cb03920d4b224e3a1732a7e24
Daniel Hoeflehner <daniel.hoeflehner@outlook.com>
2022-06-26 23:08:10 +0200

    Update dependencies

--- MovieDB.Tests/AutoMapperMovieProfileTests.cs
+++ MovieDB.Tests/AutoMapperMovieProfileTests.cs
@@ -7,101 +7,100 @@ using MovieDB.Tests.Factories;
 using Xunit;
 using UpdateRequest = MovieDB.Shared.Models.Movies.UpdateRequest;
 
-namespace MovieDB.Tests
+namespace MovieDB.Tests;
+
+public class AutoMapperMovieProfileTests
 {
-    public class AutoMapperMovieProfileTests
+    private readonly IMapper _mapper;
+
+    public AutoMapperMovieProfileTests()
     {
-        private readonly IMapper _mapper;
-
-        public AutoMapperMovieProfileTests()
+        if (_mapper is null)
         {
-            if (_mapper is null)
+            var mappingConfig = new MapperConfiguration(cfg =>
             {
-                var mappingConfig = new MapperConfiguration(cfg =>
-                {
-                    cfg.AddProfile(new MovieProfile());
-                });
+                cfg.AddProfile(new MovieProfile());
+            });
 
-                _mapper = mappingConfig.CreateMapper();
-            }
+            _mapper = mappingConfig.CreateMapper();
         }
+    }
 
-        [Fact]
-        public void AutoMapper_Movie_To_MovieResponse()
+    [Fact]
+    public void AutoMapper_Movie_To_MovieResponse()
+    {
+        var account = AccountFactory.CreateAccount();
+        var movie = MovieFactory.CreateMovie(account);
+
+        var response = _mapper.Map<MovieResponse>(movie);
+
+        Assert.Equal("Star Wars", response.Title);
+        Assert.Equal(movie.SeenAt, response.SeenAt);
+        Assert.Equal(movie.ImdbIdentifier, response.ImdbIdentifier);
+        Assert.Equal((int)movie.Rating, response.Rating);
+    }
+
+    [Fact]
+    public void AutoMapper_UpdateRequest_To_Movie()
+    {
+        var account = AccountFactory.CreateAccount();
+        var movie = MovieFactory.CreateMovie(account);
+
+        var model = new UpdateRequest
         {
-            var account = AccountFactory.CreateAccount();
-            var movie = MovieFactory.CreateMovie(account);
+            Title = "Star Wars 2",
+        };
 
-            var response = _mapper.Map<MovieResponse>(movie);
+        var updatedMovie1 = _mapper.Map(model, movie);
 
-            Assert.Equal("Star Wars", response.Title);
-            Assert.Equal(movie.SeenAt, response.SeenAt);
-            Assert.Equal(movie.ImdbIdentifier, response.ImdbIdentifier);
-            Assert.Equal((int)movie.Rating, response.Rating);
-        }
+        Assert.Equal("Star Wars 2", updatedMovie1.Title);
+        Assert.Equal(movie.SeenAt, updatedMovie1.SeenAt);
+        Assert.Equal(movie.ImdbIdentifier, updatedMovie1.ImdbIdentifier);
+        Assert.Equal(movie.Genre, updatedMovie1.Genre);
+        Assert.Equal(movie.Rating, updatedMovie1.Rating);
 
-        [Fact]
-        public void AutoMapper_UpdateRequest_To_Movie()
+        // ---
+
+        var newDate = DateTime.UtcNow.AddDays(-1);
+        var model2 = new UpdateRequest
         {
-            var account = AccountFactory.CreateAccount();
-            var movie = MovieFactory.CreateMovie(account);
+            Title = "Star Wars 3",
+            SeenAt = newDate
+        };
 
-            var model = new UpdateRequest
-            {
-                Title = "Star Wars 2",
-            };
+        var updatedMovie2 = _mapper.Map(model2, movie);
 
-            var updatedMovie1 = _mapper.Map(model, movie);
+        Assert.Equal("Star Wars 3", updatedMovie2.Title);
+        Assert.Equal(newDate, updatedMovie2.SeenAt);
 
-            Assert.Equal("Star Wars 2", updatedMovie1.Title);
-            Assert.Equal(movie.SeenAt, updatedMovie1.SeenAt);
-            Assert.Equal(movie.ImdbIdentifier, updatedMovie1.ImdbIdentifier);
-            Assert.Equal(movie.Genre, updatedMovie1.Genre);
-            Assert.Equal(movie.Rating, updatedMovie1.Rating);
+        // ---
 
-            // ---
+        var model3 = new UpdateRequest
+        {
+            Title = "Star Wars 4",
+            Genre = 0,
+            Rating = 4
+        };
 
-            var newDate = DateTime.UtcNow.AddDays(-1);
-            var model2 = new UpdateRequest
-            {
-                Title = "Star Wars 3",
-                SeenAt = newDate
-            };
+        var updatedMovie3 = _mapper.Map(model3, movie);
 
-            var updatedMovie2 = _mapper.Map(model2, movie);
+        Assert.Equal("Star Wars 4", updatedMovie3.Title);
+        Assert.Equal(MovieGenre.Action, updatedMovie3.Genre);
+        Assert.Equal(Rating.Good, updatedMovie3.Rating);
 
-            Assert.Equal("Star Wars 3", updatedMovie2.Title);
-            Assert.Equal(newDate, updatedMovie2.SeenAt);
+        // ---
 
-            // ---
+        var model4 = new UpdateRequest
+        {
+            Title = "",
+            Genre = 0,
+            Rating = 5
+        };
 
-            var model3 = new UpdateRequest
-            {
-                Title = "Star Wars 4",
-                Genre = 0,
-                Rating = 4
-            };
+        var updatedMovie4 = _mapper.Map(model4, movie);
 
-            var updatedMovie3 = _mapper.Map(model3, movie);
-
-            Assert.Equal("Star Wars 4", updatedMovie3.Title);
-            Assert.Equal(MovieGenre.Action, updatedMovie3.Genre);
-            Assert.Equal(Rating.Good, updatedMovie3.Rating);
-
-            // ---
-
-            var model4 = new UpdateRequest
-            {
-                Title = "",
-                Genre = 0,
-                Rating = 5
-            };
-
-            var updatedMovie4 = _mapper.Map(model4, movie);
-
-            Assert.Equal(movie.Title, updatedMovie4.Title);
-            Assert.Equal(MovieGenre.Action, updatedMovie4.Genre);
-            Assert.Equal(movie.Rating, updatedMovie4.Rating);
-        }
+        Assert.Equal(movie.Title, updatedMovie4.Title);
+        Assert.Equal(MovieGenre.Action, updatedMovie4.Genre);
+        Assert.Equal(movie.Rating, updatedMovie4.Rating);
     }
 }
